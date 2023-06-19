3d468b8506439e70b0f801ee231996d9f9ac04e1
Ngaiti <henfanar@gmail.com>
2023-06-19 11:26:07 +0800

    Added Profile header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import IconButton from "./components/IconButton";
+import ProfileHeader from "./components/ProfileHeader";
+import { createContext } from "react";
+import { PROFILE_DATA } from "./data";
+
+export const ProfileContext = createContext(null);
 
 export default function App() {
   return (
-    <Row>
-      <Col
-        sm={1}
-        className="d-flex flex-column justify-content-start align-items-center vh-100 bg-light"
-        style={{ position: "sticky", top: 0 }}
-      >
-        <IconButton className="bi bi-house" isTop />
-        <IconButton className="bi bi-search" />
-        <IconButton className="bi bi-compass" />
-        <IconButton className="bi bi-film" />
-        <IconButton className="bi bi-chat" />
-        <IconButton className="bi bi-heart" />
-        <IconButton className="bi bi-plus-square" />
-        <IconButton className="bi bi-person-circle" />
-        <IconButton className="bi bi-list" isBottom />
-      </Col>
-      <Col sm={11}>Main</Col>
-    </Row>
+    <ProfileContext.Provider value={PROFILE_DATA}>
+      <Row>
+        <Col
+          sm={1}
+          className="d-flex flex-column justify-content-start align-items-center vh-100 bg-light"
+          style={{ position: "sticky", top: 0 }}
+        >
+          <IconButton className="bi bi-house" isTop />
+          <IconButton className="bi bi-search" />
+          <IconButton className="bi bi-compass" />
+          <IconButton className="bi bi-film" />
+          <IconButton className="bi bi-chat" />
+          <IconButton className="bi bi-heart" />
+          <IconButton className="bi bi-plus-square" />
+          <IconButton className="bi bi-person-circle" />
+          <IconButton className="bi bi-list" isBottom />
+        </Col>
+        <Col sm={11}>
+          <ProfileHeader />
+        </Col>
+      </Row>
+    </ProfileContext.Provider>
   )
 }
