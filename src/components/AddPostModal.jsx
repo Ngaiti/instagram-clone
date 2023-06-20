b87b25ec7782817be3ab1a370d6f64708c0c8560
Ngaiti <henfanar@gmail.com>
2023-06-20 11:16:22 +0800

    Added create post function redux

--- src/components/AddPostModal.jsx
+++ src/components/AddPostModal.jsx
@@ -1,9 +1,13 @@
 import { useContext, useState } from "react";
 import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap";
 import { ProfileContext } from "../App";
+import { useDispatch } from "react-redux";
+import { createPost } from "../features/posts/postsSlice";
+
 
 export default function AddPostModal({ show, handleClose }) {
     const { image, name } = useContext(ProfileContext)
+    const dispatch = useDispatch();
 
     const [imageUrl, setImageUrl] = useState("")
     const [caption, setCaption] = useState("");
@@ -12,6 +16,7 @@ export default function AddPostModal({ show, handleClose }) {
     const handleSubmit = (e) => {
         e.preventDefault();
         if (imageUrl) {
+            dispatch(createPost({ image: imageUrl, caption }))
             setImageUrl("");
             setCaption("")
             handleClose();
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    {
+        id: 1,
+        image: "https://picsum.photos/id/123/500/500",
+        description: "Post 1 description",
+        date: new Date().toISOString(),
+        likes: 15,
+        comments: 5,
+    },
+    {
+        id: 2,
+        image: "https://picsum.photos/id/234/500/500",
+        description: "Post 2 description",
+        date: new Date().toISOString(),
+        likes: 10,
+        comments: 3,
+    },
+    {
+        id: 3,
+        image: "https://picsum.photos/id/345/500/500",
+        description: "Post 3 description",
+        date: new Date().toISOString(),
+        likes: 8,
+        comments: 2,
+    },
+    {
+        id: 4,
+        image: "https://picsum.photos/id/456/500/500",
+        description: "Post 4 description",
+        date: new Date().toISOString(),
+        likes: 12,
+        comments: 7,
+    },
+    {
+        id: 5,
+        image: "https://picsum.photos/id/567/500/500",
+        description: "Post 5 description",
+        date: new Date().toISOString(),
+        likes: 20,
+        comments: 4,
+    },
+]
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        createPost: (state, action) => {
+            const newPost = {
+                id: Date.now(),
+                image: action.payload.image,
+                description: action.payload.description,
+                date: new Date().toISOString(),
+                likes: 0,
+                comments: 0,
+            }
+            state.push(newPost)
+        }
+    }
+})
+
+export const { createPost } = postsSlice.actions
+
+export default postsSlice.reducer;
